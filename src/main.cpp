e5aaee3fe238cca794a6575e02f7c2fd466c6a6b
jungchangdo <jcd4625@gmail.com>
2020-01-22 15:02:12 +0900

    1차 완료

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,23 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
   LightSensor.begin(BH1750::CONTINUOUS_HIGH_RES_MODE);
-  PressureSensor.begin();
+  PreTemSensor.begin();
+  HumiditySensor.begin();
   wifi_set();
   configTime(9 * 3600, 0, "pool.ntp.org", "time.nist.gov");
 }
 
 void loop() {
-  delay(3000);
+  delay(30000);
   time_t now = time(nullptr);
-  getTextTime(now);
   light = LightSensor.readLightLevel();
-  pressure = PressureSensor.readPressure();
-  Serial.println(light);
-  Serial.println(pressure);
-  // cursor->execute(INSERT_SQL); 
+  pressure = PreTemSensor.readPressure();
+  temperature = PreTemSensor.readTemperature();
+  humidity = HumiditySensor.readHumidity();
+  console_print(now);
+  insert_mod();
+  // Serial.println(INSERT_SQL);
+  cursor->execute(INSERT_SQL); 
 }
 
 
@@ -44,8 +47,27 @@ void wifi_set(void) {
 void getTextTime(time_t now) {
   struct tm *timeinfo;
   timeinfo = localtime(&now);
-  String text = String(timeinfo->tm_year+1900) + " " + String(timeinfo->tm_mon+1) + " " \
-                + String(timeinfo->tm_mday) + " " + String(timeinfo->tm_hour) + ":" \
+  String text = "Y:" + String(timeinfo->tm_year+1900) + " M:" + String(timeinfo->tm_mon+1) + " D:" \
+                + String(timeinfo->tm_mday) + "  " + String(timeinfo->tm_hour) + ":" \
                 + String(timeinfo->tm_min) + ":" + String(timeinfo->tm_sec);
   Serial.println(text);
+}
+
+void console_print(time_t t) {
+  getTextTime(t);
+  Serial.println("-------------------------");
+  Serial.print("LIGHT:\t\t");
+  Serial.println(light);
+  Serial.print("PRESSURE:\t");
+  Serial.println(pressure);
+  Serial.print("TEMPERATURE:\t");
+  Serial.println(temperature);
+  Serial.print("HUMIDITY:\t");
+  Serial.println(humidity);
+  Serial.println("=========================");
+}
+
+void insert_mod() {
+  sprintf(INSERT_val, "( %.2f, %.2f, %.2f, %.2f )", light, pressure, temperature, humidity);
+  sprintf(INSERT_SQL, "%s %s", INSERT_syn, INSERT_val);
 }
--- src/main.hpp
+++ src/main.hpp
@@ -6,11 +6,15 @@
 #include <Wire.h>
 #include <BH1750.h>
 #include <Adafruit_BMP085.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
 IPAddress server_addr(192,168,10,156);
 char user[] = "node";
 char password[] = "node";
-char INSERT_SQL[] = "insert into test.hello (message) values ('hello')";
+char INSERT_syn[] = "insert into test.sensor (light, pre, tem, hum) values";
+char INSERT_val[100];
+char INSERT_SQL[150];
 
 char ssid[] = "iptime";
 char pwd[] = "";
@@ -22,8 +26,14 @@ MySQL_Cursor* cursor;
 BH1750 LightSensor(0x23);
 float light = 0.0;
 
-Adafruit_BMP085 PressureSensor;
+Adafruit_BMP085 PreTemSensor;
 float pressure = 0.0;
+float temperature = 0.0;
+
+DHT HumiditySensor(D5, DHT11);
+float humidity = 0.0;
 
 void wifi_set(void);
 void getTextTime(time_t now);
+void console_print(time_t t);
+void insert_mod(void);
